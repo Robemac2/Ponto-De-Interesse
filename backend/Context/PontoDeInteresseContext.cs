d5c385c05b735d2c1d3518e5c657e5f03af1da76
Roberto Macedo <robertoneto5@hotmail.com>
2024-12-27 18:35:30 -0300

    Added Conntext

---
+++ backend/Context/PontoDeInteresseContext.cs
@@ -0,0 +1,10 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Context
+{
+    public class PontoDeInteresseContext( DbContextOptions<PontoDeInteresseContext> options ) : DbContext(options)
+    {
+        public DbSet<PontoDeInteresse> PontosDeInteresse { get; set; }
+    }
+}
