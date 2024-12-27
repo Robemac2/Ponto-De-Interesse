5fc3de5a3a95bc96dd08fb2b968cca61b462c415
Roberto Macedo <robertoneto5@hotmail.com>
2024-12-27 18:36:06 -0300

    Added entity PontoDeInteresse

---
+++ backend/Entities/PontoDeInteresse.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entities
+{
+    [Table("Ponto-De-Interesse")]
+    public class PontoDeInteresse
+    {
+        [Key]
+        [Column("Id")]
+        [Required]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [MaxLength(50)]
+        [Required]
+        public required string Nome { get; set; }
+
+        [Column("CoordenadaX")]
+        [Range(0, int.MaxValue)]
+        [Required]
+        public int CoordenadaX { get; set; }
+
+        [Column("CoordenadaY")]
+        [Range(0, int.MaxValue)]
+        [Required]
+        public int CoordenadaY { get; set; }
+    }
+}
